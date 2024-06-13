0443652812131f9720e6b4a1c7999e20a36dbf5c
LorenzoGrande <lorenzo.grande@hotmail.it>
2024-06-13 16:37:28 +0200

    WriteDiet in working Todo: Modify menu on all scene Registration quasi complletata: rivedere entity Todo Gestire eccezioni in miglior modo

--- src/main/java/com/example/greenpear/utils/query/RegistrationQuery.java
+++ src/main/java/com/example/greenpear/utils/query/RegistrationQuery.java
@@ -3,12 +3,7 @@ package com.example.greenpear.utils.query;
 public class RegistrationQuery {
 
     static final String QUERIES_CHECK_EMAIL = "SELECT * FROM user WHERE (email = ?);";
-    static final String QUERIES_ADD_USER = "INSERT INTO user(username, email, password, role) VALUES(?,?,?, ?);"; //+
-            /*"CASE ?"+
-            "   WHEN 1 THEN 'dietitian' " +
-            "   WHEN 2 THEN 'patient' " +
-            "   ELSE RAISE_EXCPETION('Invalid value')" +
-            "END)";*/
+    static final String QUERIES_ADD_USER = "INSERT INTO user(username, email, password, role) VALUES(?,?,?, ?);";
 
     public static String addUser(){
      return QUERIES_ADD_USER;
