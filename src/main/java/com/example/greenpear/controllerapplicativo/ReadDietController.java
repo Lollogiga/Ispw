6332937d8974f863fb5d65acbd6f09a5e3756f96
lollogiga <lorenzo.grande@hotmail.it>
2024-07-18 16:21:47 +0200

    Project Finished

--- src/main/java/com/example/greenpear/controllerapplicativo/ReadDietController.java
+++ src/main/java/com/example/greenpear/controllerapplicativo/ReadDietController.java
@@ -42,41 +42,6 @@ public class ReadDietController {
         }
     }
 
-    /*public DietitianBean restoreDietitianInfo(LoginBean userBean) throws SQLException, InformationErrorException, CredentialException {
-        UserProfile currentUser = new UserProfile(userBean.getUsername());
-        Dietitian dietitian = new Dietitian();
-        DietitianBean dietitianBean;
-        dietitian.setUsername(currentUser.getUsername());
-        try{
-            DietitianDao dietitianDao = new DietitianDao();
-            dietitian = dietitianDao.getDietitianInfo(dietitian);
-            if(dietitian != null) {
-                dietitianBean = new DietitianBean(dietitian.getUsername(),
-                        dietitian.getPrice(),
-                        dietitian.getAvailable(),
-                        dietitian.getEducation(),
-                        dietitian.getWork());
-                return dietitianBean;
-            }else { return null;}
-        } catch (SQLException e) {
-            throw new SQLException(e.getMessage());
-        } catch (InformationErrorException e) {
-            throw new InformationErrorException(e.getMessage());
-        } catch (CredentialException e){
-            throw new CredentialException(e.getMessage());
-        }
-    }
-
-    public void storeDietitianInfo(DietitianBean dietitianBean) throws SQLException {
-        Dietitian dietitian = new Dietitian(dietitianBean.getUsername(), dietitianBean.getPrice(),dietitianBean.getAvailable(), dietitianBean.getPersonalEducation(), dietitianBean.getWorkExperience());
-        try{
-            DietitianDao dietitianDao = new DietitianDao();
-            dietitianDao.setDietitianInfo(dietitian);
-        } catch (SQLException e) {
-            throw new SQLException(e.getMessage());
-        }
-    }*/
-
     public RequestBean manageUpdate(LoginBean userBean) {
         DietPublisher dietPublisher = DietPublisher.getInstance();
         Request request = dietPublisher.getRequestState();
